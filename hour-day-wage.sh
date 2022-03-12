31e7c6fd8139933f9a5fe4a1f54cc3df36532a87
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-12 16:56:31 +0530

    Added Daily wages block With Calculations

--- hour-day-wage.sh
+++ hour-day-wage.sh
@@ -1,1 +1,12 @@
-echo "Hello Vivek"
+check=1;
+ispresent=$((RANDOM%2))
+full_day=8
+hourly_wages=20
+if(($ispresent == $check))
+then
+    echo "Employee is present"
+    daily_wage=$(($full_day*$hourly_wages))
+    echo " Daily Wages:$daily_wage "
+else
+    echo "Employee is Absent"
+fi
